df392b870bacff2a2a90b59e0af3c008d1d9b3d6
Navin <naveen@backstagesupporters.com>
2019-11-29 18:56:34 +0530

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,24 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  // {
+  //   path: 'tab1',
+  //   loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  // },
+  // {
+  //   path: 'tab2',
+  //   loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  // },
+  // {
+  //   path: 'tab3',
+  //   loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
+  // },
 ];
 
 @NgModule({
