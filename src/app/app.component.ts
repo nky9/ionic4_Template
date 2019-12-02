389a9a738dfe9c4608a7bf85e2c1aa0246b2d2a4
Navin <naveen@backstagesupporters.com>
2019-12-02 10:14:25 +0530

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,73 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+
+ public appPages = [
+ {
+      title: 'Home',
+      subtitle:'',
+      url: '/',
+      src:'assets/menu/home.png'
+
+    },
+  
+    {
+      title: 'Shop By Category',
+      url: 'kitchenappliance',
+      src:'assets/menu/home.png'
+    },
+    {
+      title: 'Your Account',
+       subtitle:'Login & Security',
+      url: '',
+     src:'assets/menu/home.png'
+    },
+    {
+      title: 'Your Orders',
+      subtitle:'Your address, Your returns, Order details ',
+      url: '',
+     src:'assets/menu/home.png'
+    },
+    {
+      title: 'Your Wishlist',
+      subtitle:'View your  list here ',
+      url: '/wishlist',
+     src:'assets/menu/home.png'
+    },
+    {
+      title: 'Your Payments',
+      subtitle:'Saved, Atomatic & other payment details ',
+      url: '',
+    src:'assets/menu/home.png'
+    },
+    {
+      title: 'Track Your Order',
+     
+      url: '',
+     src:'assets/menu/home.png'
+    },
+    {
+      title: 'Customer Support',
+     
+      url: '',
+     src:'assets/menu/home.png'
+    },
+    {
+      title: 'Logout',
+       url: '/login',
+     src:'assets/menu/home.png'
+    },
+   
+   
+  ];
+
+
+
+
+
+
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab1Page implements OnInit {
 
+
+// =========== slider js ==============
+	slideOpts = {
+    autoplay: {
+      delay: 4000,
+
+    },
+    zoom: false,
+    effect: 'flip',
+    spaceBetween: 10,
+     slidesPerView: 1.2,
+  };
+
+// =========== end slider js ==============
+
+ // ===========trending slider js ==============
+   sliderConfig2 = {
+    zoom: false,
+    slidesPerView: 2.3,
+    spaceBetween: 3,
+    centeredSlides: false
+  };
+// =========== end slider js ==============
   constructor() { }
 
   ngOnInit() {
